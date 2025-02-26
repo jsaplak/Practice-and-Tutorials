ed62a71607a5144eee3ece34c0725e7a5cae600a
jsaplak <juliansaplak@gmail.com>
2025-02-25 22:13:25 -0500

    added player class and allowed movement

--- python/pygame/core.py
+++ python/pygame/core.py
@@ -11,13 +11,35 @@ from pygame.locals import(
         )
 
 
-pygame.init()
+
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5, 0)
+
+
+
+pygame.init()
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+player = Player()
 
 running = True
 
@@ -30,13 +52,21 @@ while running:
         elif event.type == QUIT:
             running = False
 
+        pressed_keys = pygame.key.get_pressed()
 
-screen.fill((255, 255, 255))
+        player.update(pressed_keys)
 
-surf = pygame.Surface((50, 50))
+        screen.fill((0, 0, 0))
 
-surf.fill((0, 0, 0))
-rect = surf.get_rect()
 
-screen.blit(surf, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
-pygame.display.flip()
+        # screen.blit(player.surf, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2)) 
+        screen.blit(player.surf, player.rect)
+        # surf_center = (
+        #     (SCREEN_WIDTH-surf.get_width())/2,
+        #     (SCREEN_HEIGHT-surf.get_height())/2
+        # )
+
+        # screen.blit(surf, surf_center)
+        pygame.display.flip()
+
+      
