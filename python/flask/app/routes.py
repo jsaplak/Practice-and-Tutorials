aadeec11c5ebe5af4964e897c568ccb66da6a70b
jsaplak <juliansaplak@gmail.com>
2025-03-16 20:19:53 -0400

    Creating page templates

--- python/flask/app/routes.py
+++ python/flask/app/routes.py
@@ -1,6 +1,10 @@
+from flask import render_template
 from app import app 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Julian'}
+    return render_template('index.html', title='home', user=user)
+
+
